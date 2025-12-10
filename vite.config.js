e2edf15d7adb2c2a6a80d0d35490c76924e8b0d2
Wawans <wakuadratn@gmail.com>
2025-12-10 16:00:09 +0700

    add inertiajs

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
@@ -8,8 +9,14 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        react(),
         tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
+        },
+    },
     server: {
         watch: {
             ignored: ['**/storage/framework/views/**'],
